c09a29965186077af1163aa3114936d9d930b395
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-31 18:41:44 -0300

    Esqueleto do signup adicionado

---
+++ src/components/signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/css/signup.css";
+
+const Signup = () => {
+  return (
+    <main className="signin">
+      {/* <SigninProvider /> */}
+    </main>
+  );
+};
+
+export default Signup;
