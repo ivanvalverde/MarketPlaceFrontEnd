7f2b55f6043d319857dbba47b8ecfd0206de7a41
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-31 19:21:23 -0300

    Base do Signup

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/index";
 import Footer from "./components/footer/index";
 import Home from "./components/home/index";
-import Signin from "./components/signin/index";
-
+// import Signin from "./components/signin/index";
+import Signup from "./components/signup";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
 
         <Switch>
           <Route path="/cadastro">
             <Home />
           </Route>
           <Route exact path="/">
-            <Signin />
+            <Signup />
           </Route>
         </Switch>
 
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "../../assets/css/signup.css";
+import Form from "react-bootstrap/Form";
+import SignupTitle from "./SignupTitle";
+import SignupFooter from "./SignupFooter";
 
 const Signup = () => {
   return (
-    <main className="signin">
-      {/* <SigninProvider /> */}
+    <main className="signup">
+      <Form className="formProvider">
+      <SignupTitle />
+      <section className="inputs">
+        <Form.Group controlId="formBasicEmail" className="groupEmail">
+          <Form.Control type="email" placeholder="Email" required />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword" className="groupPassword">
+          <Form.Control type="password" placeholder="Senha" required />
+        </Form.Group>
+      </section>
+      <SignupFooter />
+    </Form>
     </main>
   );
 };
