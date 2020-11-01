579014982cd957e305efe960e662e5a4985182f9
ladeirakelwin <ladeirakelwin@gmail.com>
2020-11-01 11:50:19 -0300

    Responsividade do header adicionada

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
 
         <Switch>
           <Route path="/cadastro">
