b64d3d0372fc902833e042d213459e00aa686d71
ladeirakelwin <ladeirakelwin@gmail.com>
2020-11-04 22:00:58 -0300

    Estilizacao rota signin

--- src/components/signup/SignupTitle.jsx
+++ src/components/signup/SignupTitle.jsx
@@ -5,7 +5,7 @@ import Form from "react-bootstrap/Form";
 const SigninTitle = () => {
   return (
     <Form.Group className="groupTitle">
-      <h2 className="formTitle">Bem-vindo(a) de volta!
+      <h2 className="formTitle text-center text-dark font-weight-bold">Bem-vindo(a) de volta!
 Faça seu login:</h2>
     </Form.Group>
   );
